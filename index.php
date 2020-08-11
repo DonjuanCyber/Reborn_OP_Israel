21bb0b5b0cc2bee638725eae5456a7f2625e8247
Satrya Mahaputra <trexliar@gmail.com>
2020-08-11 20:14:30 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,121 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>#OPIsraelReborn | Indonesian Fighter Cyber Big Family</title>
+	<script>alert("We Will Kill Your Sistem")</script>
+
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+
+	<style type="text/css">
+		@font-face {
+			font-family: robot-bold;
+			src: url('https://progrez.cloud/hall-of-fame/css/font.css');
+		}
+		#bg{
+			background-image: url('https://progrez.cloud/hall-of-fame/css/bg/h.jpg');
+		}
+		.font-head{
+			background: -webkit-linear-gradient(red, white);
+			-webkit-background-clip: text;
+			-webkit-text-fill-color: transparent;
+		}
+		.font-body{
+			background: -webkit-linear-gradient(white, #38495a);
+			-webkit-background-clip: text;
+			-webkit-text-fill-color: transparent;
+		}
+	</style>
+</head>
+<body id="bg">
+	<img class="img img-responsive d-block mx-auto" src="https://i.ibb.co/SdZyNjS/ifc.png" style="padding-top: 2rem;" />
+
+	<h1 class="text-white text-center font-head" style="font-size: 3rem;">INDONESIAN FIGHTER CYBER</h1>
+	<h1 class="text-white text-center font-body" style="font-size: 2rem;">WE ARE COMEBACK!</h1>
+	<h1 class="text-white text-center font-body" style="font-size: 2rem;">#OPIsraelReborn</h1>
+
+	<h4 class="text-white text-center">We Will Not Stop Doing This</h4>
+	<h4 class="text-white text-center">Before You Give Freedom To Palestine!</h4>
+
+	<h2 class="text-white text-center blinking">MAKE DELIVERANCE or FIGHT WITH US</h2>
+	<br>
+	<br>
+	<h4 class="text-white text-center">Special Thank's To :</h4>
+	<div class="row" style="padding-bottom: 2rem;">
+		<div class="col-lg-2">
+			<img class="img img-responsive d-block mx-auto" src="https://i.ibb.co/56tZtXd/IMG-20200810-WA0015.jpg" style="padding-top: 2rem; width: 10rem;" />
+		</div>
+		<div class="col-lg-2">
+			<img class="img img-responsive d-block mx-auto" src="https://i.ibb.co/QmjvB9Z/IMG-20200810-WA0014.jpg" style="padding-top: 2rem; width: 10rem;" />
+		</div>
+		<div class="col-lg-2">
+			<img class="img img-responsive d-block mx-auto" src="https://i.ibb.co/vZPPhY9/IMG-20200810-WA0013.jpg" style="padding-top: 2rem; width: 10rem;" />
+		</div>
+		<div class="col-lg-2">
+			<img class="img img-responsive d-block mx-auto" src="https://pbs.twimg.com/media/EZ2pYz2UMAANB5c.jpg" style="padding-top: 2rem; width: 10rem;" />
+		</div>
+		<div class="col-lg-2">
+			<img class="img img-responsive d-block mx-auto" src="https://i.ibb.co/YBt46jm/IMG-20200811-WA0017.jpg" style="padding-top: 2rem; width: 10rem;" />
+		</div>
+		<div class="col-lg-2">
+			<img class="img img-responsive d-block mx-auto" src="https://i.ibb.co/QCP2ZhZ/IMG-20200811-WA0016.jpg" style="padding-top: 2rem; width: 10rem;" />
+		</div>
+		<div class="col-lg-2">
+			<img class="img img-responsive d-block mx-auto" src="https://i.ibb.co/VJxtN8m/IMG-20200811-WA0010.jpg" style="padding-top: 2rem; width: 10rem;" />
+		</div>
+		<div class="col-lg-2">
+			<img class="img img-responsive d-block mx-auto" src="https://i.ibb.co/5h12KZH/IMG-20200811-WA0011.jpg" style="padding-top: 2rem; width: 10rem;" />
+		</div>
+		<div class="col-lg-2">
+			<img class="img img-responsive d-block mx-auto" src="https://i.ibb.co/Kq74dRS/IMG-20200811-WA0012.jpg.jpg" style="padding-top: 2rem; width: 10rem;" />
+		</div>
+		<div class="col-lg-2">
+			<img class="img img-responsive d-block mx-auto" src="https://i.ibb.co/rcPSwGj/IMG-20200811-WA0013.jpg.jpg" style="padding-top: 2rem; width: 10rem;" />
+		</div>
+		<div class="col-lg-2">
+			<img class="img img-responsive d-block mx-auto" src="https://i.ibb.co/dfwWWCB/IMG-20200811-WA0014.jpg" style="padding-top: 2rem; width: 10rem;" />
+		</div>
+		<div class="col-lg-2">
+			<img class="img img-responsive d-block mx-auto" src="https://i.ibb.co/BNdc94G/IMG-20200811-WA0015.jpg.jpg" style="padding-top: 2rem; width: 10rem;" />
+		</div>
+	</div>
+
+	
+	
+	<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+	  <div class="modal-dialog " role="document">
+	    <div class="modal-content">
+	      <div class="modal-header bg-dark">
+	      	<h4 class="text-white">U Got Hacked!</h4>
+	        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+	          <span aria-hidden="true">×</span>
+	        </button>
+	      </div>
+	      <div class="modal-body bg-dark">
+			<img class="img img-responsive d-block mx-auto" src="https://i.ibb.co/Cb2cWtV/israel-ar-bendera-israel.gif" style="width: 20rem;" />
+	      </div>
+	    </div>
+	  </div>
+	</div>
+
+<script type="text/javascript">
+    $(document).ready(function(){
+        $("#exampleModal").modal('show');
+    });
+</script>
+
+<script>
+function blinker() {
+	$('.blinking').fadeOut(500);
+	$('.blinking').fadeIn(500);
+}
+setInterval(blinker, 1000);
+</script>
+	
+
+
+</body>
+</html>
